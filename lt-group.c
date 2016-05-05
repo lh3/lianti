461d39cfe5665a45cb2991dbb0f3768c45c0638c
Heng Li <lh3@me.com>
2016-05-05 10:40:28 -0400

    backup

--- lt-group.c
+++ lt-group.c
@@ -10,7 +10,7 @@ typedef struct {
 
 typedef struct {
 	uint32_t tid, st, en;
-	uint32_t is_rev:1, is_closed:1;
+	uint32_t is_rev:1, is_del:1;
 	int n_frag;
 } lt_group_t;
 
@@ -18,6 +18,7 @@ KDQ_INIT(lt_group_t)
 
 typedef struct {
 	kdq_t(lt_group_t) *q;
+	kdq_t(lt_group_t) *r;
 } lt_groups_t;
 
 void lt_opt_init(lt_opt_t *opt)
@@ -31,20 +32,64 @@ lt_groups_t *lt_grp_init(void)
 	lt_groups_t *g;
 	g = (lt_groups_t*)calloc(1, sizeof(lt_groups_t));
 	g->q = kdq_init(lt_group_t);
+	g->r = kdq_init(lt_group_t);
 	return g;
 }
 
 void lt_grp_destroy(lt_groups_t *g)
 {
+	kdq_destroy(lt_group_t, g->r);
 	kdq_destroy(lt_group_t, g->q);
 	free(g);
 }
 
-void lt_grp_push(const lt_opt_t *opt, lt_groups_t *g, const bam_hdr_t *h, const bam1_t *b)
+void lt_grp_push_region(const lt_opt_t *opt, const bam_hdr_t *h, lt_groups_t *g, const lt_group_t *r)
 {
-	int i, st, en, is_rev, is_closed = 0;
+	lt_group_t *last = kdq_size(g->r)? &kdq_last(g->r) : 0;
+	if (r == 0 && last == 0) return;
+	if (last != 0 && (r == 0 || r->tid != last->tid || r->st >= last->en)) {
+		int i, j;
+		// pass 1: merge obvious forward-reverse fragments
+		for (i = 0; i < kdq_size(g->r); ++i) {
+			lt_group_t *q = &kdq_at(g->r, i);
+			for (j = i + 1; j < kdq_size(g->r); ++j) {
+				lt_group_t *p = &kdq_at(g->r, j);
+				if (q->en <= p->st) break;
+				if (p->is_del) continue;
+				if (!q->is_rev && p->is_rev && q->en == p->en) { // merge
+					q->n_frag += p->n_frag;
+					p->is_del = 1;
+				} else if (q->is_rev != p->is_rev && q->st == p->st) {
+					if (q->en > p->en) {
+						if (q->is_rev) {
+							q->n_frag += p->n_frag;
+							p->is_del = 1;
+						}
+					} else {
+						if (p->is_rev) {
+							p->n_frag += q->n_frag;
+							q->is_del = 1;
+							break;
+						}
+					}
+				}
+			}
+		}
+		// print out
+		while (kdq_size(g->r)) {
+			lt_group_t *p = kdq_shift(lt_group_t, g->r);
+			if (!p->is_del)
+				printf("%s\t%d\t%d\t*\t%c\t%d\n", h->target_name[p->tid], p->st, p->en, "+-"[p->is_rev], p->n_frag);
+		}
+	}
+	kdq_push(lt_group_t, g->r, *r);
+}
+
+void lt_grp_push_read(const lt_opt_t *opt, lt_groups_t *g, const bam_hdr_t *h, const bam1_t *b)
+{
+	int i, st, en, is_rev;
 	const bam1_core_t *c = &b->core;
-	const uint8_t *SA, *YD;
+	const uint8_t *SA;
 
 	// compute st, en and rev
 	if (c->flag & 4) return; // unmapped
@@ -61,54 +106,29 @@ void lt_grp_push(const lt_opt_t *opt, lt_groups_t *g, const bam_hdr_t *h, const 
 		en = st + bam_cigar2rlen(c->n_cigar, bam_get_cigar(b));
 		is_rev = (c->flag & 16)? 1 : 0;
 	}
-	YD = bam_aux_get(b, "YD");
-	is_closed = YD? bam_aux2i(YD) : 0;
-	is_closed = 0;
 
 	while (kdq_size(g->q)) {
 		lt_group_t *p = &kdq_first(g->q);
 		if (p->tid != c->tid || p->en <= st) {
-			printf("%s\t%d\t%d\t%d:%d\t%c\n", h->target_name[p->tid], p->st, p->en, p->n_frag, p->is_closed, "+-"[p->is_rev]);
+			//printf("%s\t%d\t%d\t*\t%c\t%d\n", h->target_name[p->tid], p->st, p->en, "+-"[p->is_rev], p->n_frag);
+			lt_grp_push_region(opt, h, g, p);
 			kdq_shift(lt_group_t, g->q);
 		} else break;
 	}
 	for (i = 0; i < kdq_size(g->q); ++i) {
 		lt_group_t *p = &kdq_at(g->q, i);
 		int added = 0;
-		if (p->is_closed) {
-			if (is_closed) {
-				if (st == p->st && en == p->en) added = 1;
-			} else if (!is_rev) {
-				if (st == p->st && en <= p->en) added = 1;
-			} else {
-				if (en == p->en) added = 1;
-			}
+		if (!p->is_rev) {
+			if (st == p->st) added = 1, ++p->n_frag, p->en = p->en > en? p->en : en;
 		} else {
-			if (is_closed) {
-				if (!p->is_rev) {
-					if (st == p->st && en >= p->en) added = 1, p->is_closed = 1;
-				} else {
-					if (en == p->en) added = 1, p->is_closed = 1;
-				}
-			} else {
-				if (!p->is_rev) {
-					if (st == p->st) added = 1, p->en = p->en > en? p->en : en;
-				} else {
-					if (en == p->en) added = 1;
-				}
-				if (!added) {
-				}
-			}
+			if (en == p->en) added = 1, ++p->n_frag;
 		}
-		if (added) {
-			++p->n_frag;
-			break;
-		}
+		if (added) break;
 	}
 	if (i == kdq_size(g->q)) {
 		lt_group_t *p;
 		p = kdq_pushp(lt_group_t, g->q);
-		p->tid = c->tid, p->st = st, p->en = en, p->is_rev = is_rev, p->is_closed = is_closed, p->n_frag = 1;
+		p->tid = c->tid, p->st = st, p->en = en, p->is_rev = is_rev, p->n_frag = 1;
 	}
 }
 
@@ -137,7 +157,7 @@ int main(int argc, char *argv[])
 	g = lt_grp_init();
 	b = bam_init1();
 	while (bam_read1(fp, b) >= 0)
-		lt_grp_push(&opt, g, h, b);
+		lt_grp_push_read(&opt, g, h, b);
 	bam_destroy1(b);
 	lt_grp_destroy(g);
 
