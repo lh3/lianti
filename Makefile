b63c3577b9c4a68496d8735993d9456b5e439ada
Heng Li <lh3@me.com>
2016-05-06 11:32:20 -0400

    barcode aware dedup

--- Makefile
+++ Makefile
@@ -1,6 +1,6 @@
 CC=gcc
 CFLAGS=-g -Wall -O2 -Wno-unused-function
-PROG=lt-trim lt-group
+PROG=lt-trim lt-ldup lt-group
 
 .c.o:
 		$(CC) -c $(CFLAGS) $(CPPFLAGS) $(INCLUDES) $< -o $@
@@ -8,6 +8,9 @@ PROG=lt-trim lt-group
 all:$(PROG)
 
 lt-trim:lt-trim.o kthread.o
+		$(CC) $(CFLAGS) $^ -o $@ -lz -lm -lpthread
+
+lt-ldup:lt-ldup.o bgzf.o hts.o sam.o
 		$(CC) $(CFLAGS) $^ -o $@ -lz -lm -lpthread
 
 lt-group:lt-group.o bgzf.o hts.o sam.o
@@ -21,4 +24,9 @@ depend:
 
 # DO NOT DELETE
 
+bgzf.o: bgzf.h
+hts.o: bgzf.h hts.h kseq.h khash.h ksort.h
+lt-group.o: sam.h bgzf.h hts.h kdq.h
+lt-ldup.o: sam.h bgzf.h hts.h kdq.h khash.h
 lt-trim.o: kvec.h khash.h kseq.h
+sam.o: sam.h bgzf.h hts.h khash.h kseq.h kstring.h
---
+++ lt-ldup.c
@@ -0,0 +1,151 @@
+#include <zlib.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include "sam.h"
+
+typedef struct {
+	bam1_t *b;
+	uint8_t is_pe, is_left, full_ovlp;
+} elem_t;
+
+#include "kdq.h"
+KDQ_INIT(elem_t)
+
+#include "khash.h"
+KHASH_SET_INIT_INT64(64)
+KHASH_SET_INIT_STR(s)
+
+#define AUX_REALLOC_SIZE 1024
+
+static uint64_t lt_n_frags, lt_n_dups;
+
+static inline uint64_t X31_hash_string(const char *s)
+{
+	uint64_t h = *s;
+	if (h) for (++s ; *s; ++s) h = (h << 5) - h + *s;
+	return h;
+}
+
+static khash_t(64) *process(kdq_t(elem_t) *q, BGZF *fp, khash_t(s) *marked, khash_t(64) *aux)
+{
+	int i, absent;
+	if (kh_n_buckets(aux) < kdq_size(q) * 4 && kh_n_buckets(aux) <= AUX_REALLOC_SIZE) {
+		kh_clear(64, aux);
+	} else {
+		kh_destroy(64, aux);
+		aux = kh_init(64);
+	}
+	for (i = 0; i < kdq_size(q); ++i) {
+		elem_t *e = &kdq_at(q, i);
+		bam1_t *b = e->b;
+		char *qname = bam_get_qname(b);
+		if (b->core.flag&0x800) continue;
+		if ((b->core.flag&1) && (b->core.flag&(BAM_FREAD1|BAM_FREAD2))) { // PE
+			e->is_pe = 1;
+			e->full_ovlp = (b->core.tid == b->core.mtid && b->core.pos == b->core.mpos);
+			e->is_left = (b->core.tid < b->core.mtid || (b->core.tid == b->core.mtid && b->core.pos < b->core.mpos));
+		} else { // SE
+			e->is_left = 1;
+			e->is_pe = e->full_ovlp = 0;
+		}
+		if (e->is_left) {
+			const uint8_t *BC = 0;
+			uint64_t key;
+			khint_t k;
+			++lt_n_frags;
+			BC = bam_aux_get(b, "BC");
+			key = BC? X31_hash_string(bam_aux2Z(BC)) : 0;
+			k = kh_put(64, aux, key, &absent);
+			if (!absent) {
+				++lt_n_dups;
+				b->core.flag |= BAM_FDUP;
+				if (e->is_pe) kh_put(s, marked, strdup(qname), &absent);
+			}
+		}
+	}
+	for (i = 0; i < kdq_size(q); ++i) {
+		elem_t *e = &kdq_at(q, i);
+		bam1_t *b = e->b;
+		char *qname = bam_get_qname(b);
+		if (e->is_pe && !e->is_left) {
+			khint_t k;
+			k = kh_get(s, marked, qname);
+			if (k != kh_end(marked)) {
+				b->core.flag |= BAM_FDUP;
+				free((char*)kh_key(marked, k));
+				kh_del(s, marked, k);
+			}
+		}
+	}
+	while (kdq_size(q)) {
+		elem_t *e;
+		e = kdq_shift(elem_t, q);
+		bam_write1(fp, e->b);
+		bam_destroy1(e->b);
+	}
+	return aux;
+}
+
+int main(int argc, char *argv[])
+{
+	int c, clevel = -1, ret;
+	int last_tid = -1, last_pos = -1;
+	BGZF *fpr, *fpw;
+	bam_hdr_t *h;
+	bam1_t *b;
+	khash_t(s) *marked;
+	khash_t(64) *aux;
+	kdq_t(elem_t) *q;
+	khint_t k;
+
+	while ((c = getopt(argc, argv, "l:")) >= 0) {
+		if (c == 'l') clevel = atoi(optarg);
+	}
+	if (optind == argc) {
+		fprintf(stderr, "Usage: lt-ldup <aln.bam>\n");
+		return 1;
+	}
+
+	fpr = strcmp(argv[optind], "-")? bgzf_open(argv[optind], "r") : bgzf_dopen(fileno(stdin), "r");
+	h = bam_hdr_read(fpr);
+	if (clevel >= 0 && clevel <= 9) {
+		char mode[3];
+		sprintf(mode, "w%d", clevel);
+		fpw = bgzf_dopen(fileno(stdout), mode);
+	} else fpw = bgzf_dopen(fileno(stdout), "w");
+	bam_hdr_write(fpw, h);
+	aux = kh_init(64);
+	marked = kh_init(s);
+	q = kdq_init(elem_t);
+
+	b = bam_init1();
+	while ((ret = bam_read1(fpr, b)) >= 0) {
+		elem_t *e;
+		if (b->core.tid != last_tid || b->core.pos != last_pos) {
+			if (last_tid >= 0 && last_pos >= 0)
+				aux = process(q, fpw, marked, aux);
+			last_tid = b->core.tid, last_pos = b->core.pos;
+		}
+		e = kdq_pushp(elem_t, q);
+		e->b = bam_init1();
+		bam_copy1(e->b, b);
+	}
+	aux = process(q, fpw, marked, aux);
+	if (ret >= 0) {
+		do {
+			bam_write1(fpw, b);
+		} while (bam_read1(fpr, b) >= 0);
+	}
+	bam_destroy1(b);
+
+	kdq_destroy(elem_t, q);
+	fprintf(stderr, "[M::%s] %ld fragments; %ld duplicates; %d unpaired reads\n", __func__, (long)lt_n_frags, (long)lt_n_dups, kh_size(marked));
+	for (k = 0; k < kh_end(marked); ++k) 
+		if (kh_exist(marked, k)) free((char*)kh_key(marked, k));
+	kh_destroy(s, marked);
+	kh_destroy(64, aux);
+	bgzf_close(fpw);
+	bam_hdr_destroy(h);
+	bgzf_close(fpr);
+	return 0;
+}
