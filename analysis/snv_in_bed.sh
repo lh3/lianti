784ab5f619ecf58f807b6ad42bb179a5fa37d779
Longzhi Tan <tanlongzhi@gmail.com>
2019-02-20 17:04:21 -0500

    analysis/snv_in_bed: fixed typo

--- analysis/snv_in_bed.sh
+++ analysis/snv_in_bed.sh
@@ -7,7 +7,7 @@ bed_file="$2"
 region_number="$(grep "^NV" ${input_file} | awk -F $'\t' 'BEGIN {OFS = FS} {print $2,$3-1,$3}' | bedtools intersect -u -a stdin -b ${bed_file} | wc -l)"
 
 # total number of SNVs
-total_number="$(grep "^NV" ${f} | awk -F $'\t' 'BEGIN {OFS = FS} {print $2,$3-1,$3}' | wc -l)"
+total_number="$(grep "^NV" ${input_file} | awk -F $'\t' 'BEGIN {OFS = FS} {print $2,$3-1,$3}' | wc -l)"
 
 # fraction of SNVs overlapping with the BED file
 percentage="$(bc -l <<< ${region_number}/${total_number}*100 | awk '{printf "%f", $0}')"
